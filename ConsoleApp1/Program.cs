8de587ed0e41c283189bd791019b6d5eebb0a3c8
Пользователь <@DESKTOP-CAJR7TQ>
2022-11-13 20:14:48 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+ 
+    public static class Program 
+{ public static void Main() 
+    { int number; for (number = 100; number < 1000; number++) 
+            if (number % 100 * 10 + number / 100 == 564) 
+                Console.WriteLine(number); 
+    } 
+} 
+ 
